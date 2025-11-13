55f6565085f7e90b4a350221323a9ec43e87dc2d
Richard12434 <uni@oppenhorst.com>
2025-11-13 19:01:04 +0100

    Update game.js

--- Code/javaScriptFiles/game.js
+++ Code/javaScriptFiles/game.js
@@ -1,3 +1,9 @@
+import { entity } from './entity.js';
+import { player } from './player.js';
+import { enemy } from './enemy.js';
+import { map } from './map.js';
+import { obstacles } from './obstacles.js';
+
 class game {
     difficulty
     timesstamp
@@ -7,15 +13,18 @@ class game {
     }
 
     start() {
+        mapOne = new map(56,50,32)
+        playerOne = new player(canvas.width/2, canvas.height/2, 100, "png", 10, 56, 0, 0, 1 /*usw. */)
 
+        setInterval(5,render)
+        
     }
 
     stop() {
-
+        
     }
 
     resume() {
-
     }
 
     end() {
@@ -23,7 +32,9 @@ class game {
     }
 
     render() {
-
+        mapOne.draw()
+        playerOne.draw()
+        
     }
 
     spawnEnemy() {
