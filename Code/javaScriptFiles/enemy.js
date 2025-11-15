31516b1cdb5ff5e8f88ff8b358b19542365d733c
skrirex <johannes.schreiber00@yahoo.com>
2025-11-15 23:20:53 +0100

    Chase player +draw +die function ergänzt

--- Code/javaScriptFiles/enemy.js
+++ Code/javaScriptFiles/enemy.js
@@ -1,12 +1,4 @@
-class enemy extends entity { 
-
-    globalX
-    globalY
-    hppng
-    speed
-    hitboxlevel
-    xpDrop
-    elite
+class Enemy extends Entity {
     
     constructor(globalX, globalY, hp, png, speed, hitbox, level, xpDrop, elite){
         super(globalX, globalY, hp, png, speed, hitbox)
@@ -15,11 +7,28 @@ class enemy extends entity {
         this.elite = elite
     }
 
-    chasePlayer() {
+    chasePlayer(player) {
+        let dx = player.globalX - this.globalX
+        let dy = player.globalY - this.globalY
 
+        let distance = Math.sqrt(dx*dx+dy*dy) //Hypotenuse von Enemy zu Player berechnet distance
+        if (distance === 0) return //bleibt bei distance = 0 stehen
+
+        this.globalX+= dx * this.speed //Gegner läuft in Richtung des Players
+        this.globalY+= dy * this.speed
+        
     }
 
-    draw() {
+    draw(ctx) {
+    if (!this.png) return;
 
+    const drawX = this.globalX - this.hitbox.width / 2;
+    const drawY = this.globalY - this.hitbox.height / 2;
+
+    ctx.drawImage(this.png, drawX, drawY, this.hitbox.width, this.hitbox.height);
+}
+
+    die() {
+    console.log("Enemy ist gestorben! XP gedroppt:", this.xpDrop);
     }
 }
