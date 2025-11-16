324d0c88bf9916ce3b48747ee19976cf6533828a
skrirex <johannes.schreiber00@yahoo.com>
2025-11-16 20:55:27 +0100

    spawnEnemyAtEdge function hinzugefügt

--- Code/javaScriptFiles/enemy.js
+++ Code/javaScriptFiles/enemy.js
@@ -12,7 +12,10 @@ class Enemy extends Entity {
         let dy = player.globalY - this.globalY
 
         let distance = Math.sqrt(dx*dx+dy*dy) //Hypotenuse von Enemy zu Player berechnet distance
-        if (distance === 0) return //bleibt bei distance = 0 stehen
+        if (distance === 0) return //bleibt stehen bei distance = 0
+
+        dx /= distance; // Teilt Entfernung durch sich selbst -> Gegner bewegt sich gleichmäßig
+        dy /= distance;
 
         this.globalX+= dx * this.speed //Gegner läuft in Richtung des Players
         this.globalY+= dy * this.speed
@@ -31,4 +34,43 @@ class Enemy extends Entity {
     die() {
     console.log("Enemy ist gestorben! XP gedroppt:", this.xpDrop);
     }
+}
+
+function spawnEnemyAtEdge(mapWidth, mapHeight) {
+
+    const side = Math.floor(Math.random() * 4); 
+    let x, y;
+
+    switch (side) {
+        case 0: // oben
+            x = Math.random() * mapWidth;   // spawnt Enemy an random Stelle am oberen Rand
+            y = 0;
+            break;
+
+        case 1: // rechts
+            x = mapWidth;
+            y = Math.random() * mapHeight;
+            break;
+
+        case 2: // unten
+            x = Math.random() * mapWidth;
+            y = mapHeight;
+            break;
+
+        case 3: // links
+            x = 0;
+            y = Math.random() * mapHeight;
+            break;
+    }
+
+    // temporäre Werte, ohne lvl System bisher
+    const hp = 10;
+    const png = enemyImg;         
+    const speed = 0.5;            
+    const hitbox = { width: 10, height: 20 };
+    const level = 1;
+    const xpDrop = 2;
+    const elite = false;
+
+    return new Enemy(x, y, hp, png, speed, hitbox, level, xpDrop, elite);
 }
