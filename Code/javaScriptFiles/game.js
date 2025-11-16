89540d57df19b6cad625c1935ab00ff7cc5f3cac
Richard12434 <uni@oppenhorst.com>
2025-11-16 15:58:20 +0100

    Grundlagen der Game Class
    Die import sind bisher nicht alle notwendig aber schaden tun sie nicht. Wenn die klasse implementiert werden soll muss man sie nur entkommentieren

--- Code/javaScriptFiles/game.js
+++ Code/javaScriptFiles/game.js
@@ -1,22 +1,32 @@
-import { entity } from './entity.js';
-import { player } from './player.js';
-import { enemy } from './enemy.js';
-import { map } from './map.js';
-import { obstacles } from './obstacles.js';
+//import { dropSingleUse } from "./dropSingleUse.js"
+//import { enemy } from "./enemy.js"
+//import { entity } from "./entity.js"
+//import { equipment } from "./equipment.js"
+//import { item } from "./item.js"
+import { map } from "./map.js"
+//import { obstacles } from "./obstacles.js"
+import { player } from "./player.js"
+//import { projectile } from "./projectile.js"
+//import { projectile } from "./projectile.js"
+//import { weapon } from "./weapon.js";
+const canvas = document.getElementById('game')
+const ctx = canvas.getContext('2d')
 
-class game {
-    difficulty
+export class game {
+    difficulty=1
     timesstamp
 
     constructor() {
-
+        this.MapOne=null
+        this.PlayerOne=null
     }
 
     start() {
-        mapOne = new map(56,50,32)
-        playerOne = new player(canvas.width/2, canvas.height/2, 100, "png", 10, 56, 0, 0, 1 /*usw. */)
-
-        setInterval(5,render)
+        const timestamp = Date.now();
+        this.MapOne = new map(57,52,32,canvas.width, ctx)
+        this.PlayerOne = new player(canvas.width/2, canvas.height/2, 100, "png", 10, 56, 0, 0, 1 /*usw. */)
+        setInterval(Game.render.bind(this), 5)
+        //setInterval(spawnEnemy, 100)
         
     }
 
@@ -25,6 +35,7 @@ class game {
     }
 
     resume() {
+        
     }
 
     end() {
@@ -32,12 +43,17 @@ class game {
     }
 
     render() {
-        mapOne.draw()
-        playerOne.draw()
+        //this.PlayerOne.draw()
+        this.MapOne.draw(this.PlayerOne.globalX, this.PlayerOne.globalY)
+        
+        //enemy.draw()
         
     }
 
     spawnEnemy() {
-
+        enemy = new enemy(map.leftBorder, map.topBorder, 100, "a.png", 10, 5, 0, 5, false)
     }
 }
+
+const Game = new game()
+Game.start()
