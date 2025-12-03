e24748273cd4abab1afd5a7eff760901fc880a4d
skrirex <johannes.schreiber00@yahoo.com>
2025-12-03 19:20:31 +0100

    added heal drop item

--- Code/javaScriptFiles/dropSingleUse.js
+++ Code/javaScriptFiles/dropSingleUse.js
@@ -28,3 +28,25 @@ export class SpeedBoostDrop extends DropSingleUse {
         }, this.duration)
     }
 }
+
+    export class HealDrop extends DropSingleUse {
+
+    constructor(globalEntityX, globalEntityY) {
+        super(globalEntityX, globalEntityY)
+        this.healAmount = 20 // z.B. 20 HP heilen
+    }
+
+    use(player) {
+        if (this.used) return
+        super.use(player)
+
+        if (player == null) return
+
+        // Falls maxHp existiert, daran begrenzen
+        if (typeof player.maxHp === "number") {
+            player.hp = Math.min(player.hp + this.healAmount, player.maxHp)
+        } else {
+            player.hp += this.healAmount
+        }
+    }
+}
