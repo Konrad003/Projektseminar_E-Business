8cce162a20e79dcd8417ff032e0a975f56ff48f6
Niklas Mehlis <75626426+Der-Mehlmann@users.noreply.github.com>
2025-12-10 00:02:48 +0100

    Update enemy.js

--- Code/javaScriptFiles/enemy.js
+++ Code/javaScriptFiles/enemy.js
@@ -104,12 +104,12 @@ export class Enemy extends MovingEntity {
     
         
         enemyXpDrop.push(new DropSingleUse(this.globalEntityX, this.globalEntityY))
-    
+    }
 
 
 export const enemyItemDrop = []
 export const enemyXpDrop = []
-}
+
 export function drawEnemyItem(ctx, player, map) {
     const leftBorder = player.globalEntityX - map.FOVwidth / 2
     const topBorder = player.globalEntityY - map.FOVheight / 2
