15a5aa3f5a48a2c124e30796b78e39ac0d57565b
Richard12434 <uni@oppenhorst.com>
2026-01-12 21:40:31 +0100

    Add circular movement to EnemySchatzgoblin
    Introduces circular movement logic for EnemySchatzgoblin when not scared, using a radius and angular speed. Also reduces default speed and adds a 'scared' state to switch to chasing the player.

--- Code/javaScriptFiles/EnemySchatzgoblin.js
+++ Code/javaScriptFiles/EnemySchatzgoblin.js
@@ -4,7 +4,7 @@ export class EnemySchatzgoblin extends Enemy {
     constructor(globalEntityX, globalEntityY, hp, png, speed, hitbox, gridMapTile, oldMoveX, oldMoveY, blockedX, blockedY, weapon) {
         super(globalEntityX, globalEntityY, hp, png, speed, hitbox, gridMapTile)
         this.hp = 150
-        this.speed = 2
+        this.speed = 0.5
         this.png = "EnemySchatzgoblin"
         this.hitbox = {width: 22, height: 22}
         this.level = 1
@@ -16,6 +16,31 @@ export class EnemySchatzgoblin extends Enemy {
         this.blockedY = blockedY
         this.ranged = false
         this.weapon = null  // wenn Waffen implementiert sind, durch weapon ersetzen
+
+        this.radius = 64;
+        this.centerX = globalEntityX;
+        this.centerY = globalEntityY;
+        // Startwinkel zufällig
+        this.angle = Math.random() * Math.PI * 2;
+        this.angularSpeed = 1.0 / 60.0; // Eine Umdrehung alle 6 Sekunden
+        this.scared = false
+        }
+
+    move() {
+        if (!this.scared) {
+        this.angle += this.angularSpeed;
+        this.globalEntityX = this.centerX + Math.cos(this.angle) * this.radius;
+        this.globalEntityY = this.centerY + Math.sin(this.angle) * this.radius;
+        } else {        
+            this.chasePlayer(MapOne, PlayerOne, enemies)                   // Gegner läuft auf den Spieler zu
+        }
+        if (PlayerOne.globalEntityX -)
+    }
+
+    render(ctx, MapOne, PlayerOne, enemies, projectiles, performanceNow, positionWithin, gridWidth){  
+        let position=this.updateGridPlace(MapOne.tilelength, enemies, positionWithin, gridWidth)        
+        this.draw(ctx, PlayerOne)
+        this.move()
     }
 
     getColor() {
